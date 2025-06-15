23277349efe9f2f990634686d64dc5e124fed5ee
Eduardo-J-S <eduardojose59@hotmail.com>
2025-06-15 14:44:49 -0300

    refactor: refatora script para fazer backup automatico diário de tarefas

--- automation/src/services/projects.service.ts
+++ automation/src/services/projects.service.ts
@@ -1,5 +1,5 @@
-import { FieldSchema, parseFieldValues } from "@/field-parser/field-parser" 
-import { env } from "@/env"
+import { FieldSchema, parseFieldValues } from "../field-parser/field-parser"
+import { env } from "../env"
 
 export class ProjectsService {
   async getGroupedTasksFromProject(projectId: string, schema?: FieldSchema) {

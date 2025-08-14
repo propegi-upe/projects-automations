8a37b609e8e340041d9779c6d04a1182ebe09135
Eduardo-J-S <eduardojose59@hotmail.com>
2025-08-14 20:40:26 -0300

    feat: adiciona script de envio de emails

---
+++ emails/src/scripts/main.ts
@@ -0,0 +1,23 @@
+import { NodemailerEmailService } from "@/services/implementation/nodemailer-email-service"
+import { SendEmailUseCase } from "@/use-cases/send-email.use-case"
+import "dotenv/config"
+
+const emailData = {
+  to: "ejsilva159@gmail.com",
+  subject: "Bem-vindo!",
+  text: "Seja bem-vindo ao nosso sistema.",
+}
+
+async function main() {
+    const service = new NodemailerEmailService()
+    const useCase = new SendEmailUseCase(service)
+
+  try {
+    await useCase.execute(emailData)
+    console.log("E-mail enviado com sucesso.")
+  } catch (err) {
+    console.error("Erro ao enviar e-mail:", err)
+  }
+}
+
+main()

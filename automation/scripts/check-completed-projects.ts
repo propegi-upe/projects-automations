3ed4ca954d0fb15f7e62e6f7cb0d6071b6b2dd8a
Eduardo-J-S <eduardojose59@hotmail.com>
2025-08-16 23:05:10 -0300

    feat: implementa caso de uso `CheckCompletedProjectsUseCase` que move envia email de encerramento se card estiver na coluna `Finalizado`

---
+++ automation/scripts/check-completed-projects.ts
@@ -0,0 +1,58 @@
+import { NodemailerEmailService } from "@/services/email-service/implementations/nodemailer-email-service"
+import { HandlebarsHtmlCompiler } from "@/services/email-service/implementations/handlebars-html-compiler"
+import { SendClosureEmailUseCase } from "@/use-cases/send-closure-email.use-case"
+
+import { CheckCompletedProjectsUseCase } from "@/use-cases/check-completed-projects/check-completed-projects.use-case"
+
+const BOARD_ID = "PVT_kwDODE36584A64ML" 
+
+async function main() {
+  const checkCompletedProjectsUseCase = new CheckCompletedProjectsUseCase()
+  const emailService = new NodemailerEmailService()
+  const htmlCompiler = new HandlebarsHtmlCompiler()
+  const sendClosureEmailUseCase = new SendClosureEmailUseCase(
+    emailService,
+    htmlCompiler
+  )
+
+  const allCards = await checkCompletedProjectsUseCase.getGroupedTasksFromProject(BOARD_ID)
+
+  for (const card of allCards) {
+    const status = checkCompletedProjectsUseCase.getSingleSelectValue(card, "Status") ?? "Sem status"
+
+    if (status !== "Finalizado ✅") continue
+
+    const notificado = checkCompletedProjectsUseCase.getSingleSelectValue(card, "Notificado");
+
+    if (!notificado || notificado === "false"){
+        console.log(`Projeto ${card.title} ainda não notificado.`);
+
+        const projectName = card.content?.title ?? "Projeto sem título"
+
+        const companyName = checkCompletedProjectsUseCase.getTextValue(card, "🏛️ Empresa") ?? "Empresa"
+
+        const professorName = checkCompletedProjectsUseCase.getTextValue(card, "👤 Coordenador") ?? "Coordenador"
+      
+        const emailDestino = checkCompletedProjectsUseCase.getTextValue(card, "✉️ E-mail")
+
+        if (emailDestino) {
+        await sendClosureEmailUseCase.execute({
+          to: emailDestino,
+          projectName,
+          companyName,
+          professorName,
+        })
+            console.log(`Notificação de encerramento enviada para ${emailDestino}`)
+        } else {
+            console.warn(`Não foi possível enviar e-mail para ${projectName}, sem campo "✉️ E-mail"`)
+        }
+
+        // Atualiza o campo "Notificado" para true
+        await checkCompletedProjectsUseCase.updateCardField(card.id);
+    }
+  }
+}
+
+main().catch((e) => {
+  console.error("Erro ao executar o script:", e)
+})
---
+++ automation/src/use-cases/check-completed-projects/check-completed-projects.use-case.ts
@@ -0,0 +1,138 @@
+import { env } from '../../env'; 
+
+export class CheckCompletedProjectsUseCase  { 
+    async getGroupedTasksFromProject(projectId: string) {
+    const query = `
+    query {
+        node(id: "${projectId}") {
+          ... on ProjectV2 {
+            items(first: 100) {
+              nodes {
+                id
+                content {
+                  ... on Issue {
+                    title
+                    number
+                    url
+                  }
+                  ... on PullRequest {
+                    title
+                    number
+                    url
+                  }
+                  ... on DraftIssue {
+                    title
+                    body
+                  }
+                }
+                fieldValues(first: 30) {
+                  nodes {
+                    ... on ProjectV2ItemFieldSingleSelectValue {
+                      optionId
+                      name
+                      field {
+                        ... on ProjectV2SingleSelectField {
+                          name
+                        }
+                      }
+                    }
+                    ... on ProjectV2ItemFieldTextValue {
+                      text
+                      field {
+                        ... on ProjectV2Field {
+                          name
+                        }
+                      }
+                    }
+                    ... on ProjectV2ItemFieldDateValue {
+                      date
+                      field {
+                        ... on ProjectV2Field {
+                          name
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    `
+
+    const response = await fetch("https://api.github.com/graphql", {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${env.REPOSITORY_ACCESS_TOKEN}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ query }),
+    })
+
+    const json = await response.json()
+
+    if (json.errors) {
+      console.error(json.errors)
+      throw new Error("Erro ao buscar tarefas.")
+    }
+
+    return json.data.node.items.nodes
+  }
+
+  getTextValue(item: any, fieldName: string): string | null {
+    const field = item.fieldValues.nodes.find(
+      (f: any) => f.field?.name === fieldName && f.text !== undefined
+    )
+    return field?.text ?? null
+  }
+
+    // Retorna o valor de um campo SingleSelect com base no nome do campo
+  getSingleSelectValue(item: any, fieldName: string): string | null {
+    const field = item.fieldValues.nodes.find(
+      (f: any) => f.field?.name === fieldName && f.name !== undefined
+    )
+    return field?.name ?? null
+  }
+
+    private statusFieldId = "PVTSSF_lADODE36584A64MLzgvV8uc"
+  
+    async updateCardField(itemId: string): Promise<void> {
+      const optionId = "b0c02715" // id da opção "true"
+  
+      const mutation = `
+      mutation {
+        updateProjectV2ItemFieldValue(
+          input: {
+            projectId: "PVT_kwDODE36584A64ML"
+            itemId: "${itemId}"
+            fieldId: "${this.statusFieldId}"
+            value: {
+              singleSelectOptionId: "${optionId}"
+            }
+          }
+        ) {
+          projectV2Item {
+            id
+          }
+        }
+      }
+    `
+  
+      const response = await fetch("https://api.github.com/graphql", {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${env.REPOSITORY_ACCESS_TOKEN}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ query: mutation }),
+      })
+  
+      const json = await response.json()
+      if (json.errors) {
+        console.error(json.errors)
+        throw new Error("Erro ao atualizar status do item.")
+      }
+    }
+
+} 

fa769e6c14e1f0a6365e1afbfa42c00eb2d5a2b3
Eduardo-J-S <eduardojose59@hotmail.com>
2025-09-15 08:10:59 -0300

    refactor: refatora app.ts

--- automation/src/app.ts
+++ automation/src/app.ts
@@ -1,12 +1,7 @@
 import Fastify from "fastify"
 import { projectsRoutes } from "./routes/projects.route"
 
-import routesFinancial from "./routes/projects-financial.route"
-import routesDT from "./routes/projects-dt.routes"
-
 export const app = Fastify()
 
 app.register(projectsRoutes)
 
-app.register(routesFinancial)
-app.register(routesDT)

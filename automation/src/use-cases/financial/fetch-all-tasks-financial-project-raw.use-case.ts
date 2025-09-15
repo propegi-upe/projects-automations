58fc207077dad85404828f9a4e1adebbfc81edbc
Eduardo-J-S <eduardojose59@hotmail.com>
2025-09-15 08:08:43 -0300

    refactor: deleta arquivos para iniciar organização da arquitetura

--- automation/src/use-cases/financial/fetch-all-tasks-financial-project-raw.use-case.ts
+++
@@ -1,9 +0,0 @@
-import { ProjectsService } from "@/services/projects.service"
-
-// Lista todas as tasks de um projeto com organização
-export async function getAllTasksFinancialProjectRawUseCase(
-  projectId: string,
-) {
-  const service = new ProjectsService()
-  return await service.getGroupedTasksFromProject(projectId)
-}
